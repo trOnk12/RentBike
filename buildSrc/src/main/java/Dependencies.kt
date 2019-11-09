ada25f1adc202b19d1dc0df5ced1e635baeda77a
trOnk12 <m.pachulski94@gmail.com>
2019-11-09 12:19:31 +0100

    dependencies moved to external file / still can't resolve

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,17 +1,38 @@
 object Versions {
-    object Standard {
+    object DefaultConfig {
+        const val compiledSdkVersion = 29
+        const val buildToolsVersion = "29.0.2"
+        const val applicationId = "com.example.rentbike"
+        const val minSdkVersion = 21
+        const val targetSdkVersion = 29
+        const val versionCode = 1
+        const val versinName = "1.0"
+        const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
 
+    object Standard {
+        const val appCompat = "1.1.0"
+        const val coreKtx = "1.1.0"
+        const val constraintLayout = "1.1.3"
+        const val jUnit = "4.12"
+        const val testRunner = "1.2.0"
+        const val espressoCore = "3.2.0"
     }
 
     object External {
-        const val
+
 
     }
 }
 
 object Dependencies {
     object Standard {
-
+        const val appCompat = "androidx.appcompat:appcompat:${Versions.Standard.appCompat}"
+        const val coreKtx = "androidx.core:core-ktx:${Versions.Standard.coreKtx}"
+        const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.Standard.constraintLayout}"
+        const val jUnit = "junit:junit:${Versions.Standard.jUnit}"
+        const val testRunner = "androidx.test:runner:${Versions.Standard.testRunner}"
+        const val espressoCore = "androidx.test.espresso:espresso-core:${Versions.Standard.espressoCore}"
     }
 
     object External {
