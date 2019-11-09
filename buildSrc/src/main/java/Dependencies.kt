fdd463c80547e830d82e7b09653493fe33f308ed
trOnk12 <m.pachulski94@gmail.com>
2019-11-09 12:08:22 +0100

    gradle dependency managment added

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,20 @@
+object Versions {
+    object Standard {
+
+    }
+
+    object External {
+        const val
+
+    }
+}
+
+object Dependencies {
+    object Standard {
+
+    }
+
+    object External {
+
+    }
+}
