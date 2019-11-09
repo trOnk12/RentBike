cb8dfd892eece82f8ba5fe736494bd59301f8385
trOnk12 <m.pachulski94@gmail.com>
2019-11-09 12:43:12 +0100

    dependencies moved to external file / everything resolving

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,41 +1,42 @@
-object Versions {
-    object DefaultConfig {
-        const val compiledSdkVersion = 29
-        const val buildToolsVersion = "29.0.2"
-        const val applicationId = "com.example.rentbike"
-        const val minSdkVersion = 21
-        const val targetSdkVersion = 29
-        const val versionCode = 1
-        const val versinName = "1.0"
-        const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    object Standard {
-        const val appCompat = "1.1.0"
-        const val coreKtx = "1.1.0"
-        const val constraintLayout = "1.1.3"
-        const val jUnit = "4.12"
-        const val testRunner = "1.2.0"
-        const val espressoCore = "3.2.0"
-    }
-
-    object External {
-
-
-    }
+object DefaultConfig {
+    const val compiledSdkVersion = 29
+    const val buildToolsVersion = "29.0.2"
+    const val applicationId = "com.example.rentbike"
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 29
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
 
-object Dependencies {
-    object Standard {
-        const val appCompat = "androidx.appcompat:appcompat:${Versions.Standard.appCompat}"
-        const val coreKtx = "androidx.core:core-ktx:${Versions.Standard.coreKtx}"
-        const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.Standard.constraintLayout}"
-        const val jUnit = "junit:junit:${Versions.Standard.jUnit}"
-        const val testRunner = "androidx.test:runner:${Versions.Standard.testRunner}"
-        const val espressoCore = "androidx.test.espresso:espresso-core:${Versions.Standard.espressoCore}"
-    }
+object Versions {
+    const val kotlinStd = "1.3.31"
+    const val appCompat = "1.1.0"
+    const val coreKtx = "1.1.0"
+    const val constraintLayout = "1.1.3"
 
-    object External {
+    const val jUnit = "4.12"
+    const val testRunner = "1.2.0"
+    const val espressoCore = "3.2.0"
+}
 
-    }
+object StandardLibraries {
+    const val kotlinStd = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlinStd}"
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    const val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+}
+
+object TestLibaries {
+    const val jUnit = "junit:junit:${Versions.jUnit}"
+    const val testRunner = "androidx.test:runner:${Versions.testRunner}"
+    const val espressoCore = "androidx.test.espresso:espresso-core:${Versions.espressoCore}"
+}
+
+object ExternalLibaries {
+
+}
+
+object GoogleLibaries {
+
 }
